ccf7e0dd59f18f3f6275b1dbcd701ddee2cd19dd
merylschoice22 <kekrieg.digital@gmail.com>
2021-06-05 06:32:52 +0200

    DELETE message

--- server.js
+++ server.js
@@ -36,9 +36,33 @@ app.get("/messages/:id", (req, res) => {
 });
 
 //POST - Create a new message
-app.post("");
+app.post("/messages/new", (req, res) => {
+  //Create new message object
+  const lastMessageIndex = parseInt(messages.length - 1);
+  const newID = messages[lastMessageIndex].id + 1;
+  const newMessage = {
+    id: newID,
+    from: "Mom",
+    text: "Where are you? Call me!",
+  };
+  //Add it to the array of messages
+  messages.push(newMessage);
+  //Return success and console log the full array of messages
+  console.log(newMessage);
+  console.log(messages);
+  res.status(201).send(newMessage);
+});
 
 //DELETE - Delete a message, by ID
+app.delete("/messages/:id", (req, res) => {
+  //Identify message by ID and index
+  const id = parseInt(req.params.id);
+  const index = messages.findIndex((message) => message.id == id);
+  //Remove message by index from array
+  messages.splice(index, 1);
+  //Return successful deletion and console log full array of messages
+  res.status(200).send(`Message successfully`);
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
